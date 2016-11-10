6cfa927bc3123e5d2d5e82162f1b86bfd8d6e5d4
Adam Kudrna <adam@visionapps.cz>
2016-11-10 15:57:04 +0100

    Tweak some Bootstrap variables to adjust appearance of our app

--- grunt/watch.js
+++ grunt/watch.js
@@ -6,13 +6,5 @@ module.exports = {
     files: ['<%= paths.src %>/less/**/*.less'],
     tasks: ['build-css'],
   },
-  js: {
-    files: ['<%= paths.src %>/js/*.js'],
-    tasks: ['build-js'],
-  },
-  images: {
-    files: ['<%= paths.src %>/images/**/*'],
-    tasks: ['copy:images'],
-  },
 
 };
